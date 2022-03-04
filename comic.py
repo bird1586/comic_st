785faa2e17c35247141f5123732334a3afe5c680
Chingyen <42596154+bird1586@users.noreply.github.com>
2022-03-04 16:34:09 +0800

    Update comic.py

--- comic.py
+++ comic.py
@@ -9,28 +9,36 @@ from requests_html import HTMLSession
 import re
 import streamlit as st
 
-session = HTMLSession()
+@st.cache
+def get_chapters(comic):
+    r = session.get(rf'https://www.omyschool.com/article_list/{comics[comic]}/{comic}/')
+    chapters = r.html.find('div.chapter a')
+    chapters = chapters[:-24]
+    chapters = {re.findall('\d+', c.attrs['href'])[-1]:c.attrs['href'] for c in chapters}
+    return chapters
 
-comics= {'海賊王':1, '一拳超人':51, '王者天下':104, '新網球王子':32, '名偵探柯南':47}
-comic = st.selectbox('請選擇漫畫', comics)
-r = session.get(rf'https://www.omyschool.com/article_list/{comics[comic]}/{comic}/')
-chapters = r.html.find('div.chapter a')
-chapters = chapters[:-24]
-chapters = {re.findall('\d+', c.attrs['href'])[-1]:c.attrs['href'] for c in chapters}
-
-option  = st.selectbox('請選擇章節', chapters)
-
-if option:
+@st.cache
+def get_pages(chapter):
     r = session.get(r'https://www.omyschool.com' + chapters[option])
     links = r.html.find("amp-img[width='580']")
     links = [i.attrs['src'] for i in links]
+    return links
     
+
+session = HTMLSession()
+comics= {'海賊王':1, '一拳超人':51, '王者天下':104, '新網球王子':32, '名偵探柯南':47}
+comic = st.selectbox('請選擇漫畫', comics)
+chapters = get_chapters(comic)
+
+chapter  = st.selectbox('請選擇章節', chapters)
+
+if chapter:
+    links = get_pages(chapter)
     for link in links:
         st.image(link)
         
 st.markdown('''
                 <style>
-                div[data-testid="stToolbar"] {visibility: hidden;};
-                div.viewerBadge_link__1S137 {visibility: hidden;};
+                div[data-testid="stToolbar"] {visibility: hidden;}
                 </style>
                 ''', unsafe_allow_html=True)        
