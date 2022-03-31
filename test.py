3f10a80e0e78aab687575e981e2276115b626736
Chingyen <42596154+bird1586@users.noreply.github.com>
2022-03-31 13:07:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
+from bokeh.models.widgets import Div
 import streamlit as st
-import webbrowser
 
-
-
-a = st.button('google')
-if a:
-    webbrowser.open(r'http://www.google.com')
+if st.button('Go to Streamlit'):
+    js = "window.open('https://www.streamlit.io/')"  # New tab or window
+    js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
