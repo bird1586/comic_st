7eb0b28b9c662d3d8d36dddad24cb7f915915570
Chingyen <42596154+bird1586@users.noreply.github.com>
2022-03-31 13:10:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-from bokeh.models.widgets import Div
-import streamlit as st
+from link_button import link_button
 
-if st.button('Go to Streamlit'):
-    js = "window.open('https://www.streamlit.io/')"  # New tab or window
-    js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text=html)
-    st.bokeh_chart(div)
+link_button('Click Me!', 'https://docs.streamlit.io/en/stable/')
